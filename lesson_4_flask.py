c376a6bdf2c936a104002fcc245b2dd93bc4828c
Nikolay Tokariev <104066410+crowstrong@users.noreply.github.com>
2022-07-07 13:21:39 +0300

    Update lesson_4_flask.py

--- lesson_4_flask.py
+++ lesson_4_flask.py
@@ -4,6 +4,7 @@ import random
 import requests
 import pandas as pd
 
+from datetime import datetime
 from flask import Flask, jsonify, abort
 from faker import Faker
 from webargs import fields, validate
@@ -12,9 +13,10 @@ from webargs.flaskparser import use_kwargs
 fake_instance = Faker('uk_UA')
 
 app = Flask(__name__)
+app.config.update(JSON_SORT_KEYS=False)
 
 
-@app.route("/students")
+@app.route('/students')
 @use_kwargs(
     {
         'quantity': fields.Int(
@@ -27,54 +29,44 @@ app = Flask(__name__)
 def generate_students(quantity):
     students_data: list = [
         {
-            'first_name': fake_instance.first_name(),
-            'last_name': fake_instance.last_name(),
-            'email': fake_instance.unique.email(),
-            'password': fake_instance.password(length=random.randint(10, 20)),
-            'birthday': str(fake_instance.date_of_birth(minimum_age=18, maximum_age=28))
+            'First_name': fake_instance.first_name(),
+            'Last_name': fake_instance.last_name(),
+            'Email': fake_instance.unique.email(),
+            'Password': fake_instance.password(length=random.randint(10, 20)),
+            'Birthday': str(fake_instance.date_of_birth(minimum_age=18, maximum_age=28))
         } for data in range(quantity)
     ]
-    df = pd.DataFrame(columns=['First Name', 'Last Name', 'Email', 'Password', 'Birthday'])
-    for p in students_data:
-        df = pd.concat([df, pd.DataFrame(p, index=[0])], ignore_index=True)
-        df.to_csv("students_data.csv", index=False)
-
-    app.config.update(JSON_SORT_KEYS=False)
+    df = pd.DataFrame(students_data)
+    df.to_csv('students_data.csv', index=False)
 
     return jsonify(students_data)
-
-
-def currency_checking(existing, request):
-    if request not in existing:
-        return abort(400, f'Incorrect currency code. Existing codes: {existing}')
 
 
 @app.route('/btc')
 @use_kwargs(
     {
-        "count": fields.Int(
+        'count': fields.Int(
             load_default=1,
             validate=[validate.Range(min=1)]
         ),
-        "currency": fields.Str(
-            load_default="USD"
+        'currency': fields.Str(
+            load_default='USD'
         )
     },
-    location="query"
+    location='query'
 )
 def get_bitcoin_value(count, currency) -> str:
-    url: dict = requests.get("https://bitpay.com/currencies").json()
-    url: list = url['data']
+    url = requests.get('https://bitpay.com/currencies').json()
+    url = url['data']
 
-    currency_codes: list = [currency['code'] for currency in url]
+    currency_codes = [currency['code'] for currency in url]
+    if currency not in currency_codes:
+        abort(400, f'Incorrect currency code. Existing codes: {currency}')
 
-    currency_checking(currency_codes, currency)
-
-    response: dict = requests.get(f"https://bitpay.com/api/rates/{currency}").json()
-    rate: float = round(response["rate"] * int(count), 2)
-    symbol: str = next(bitpay_currency['symbol'] for bitpay_currency in url if bitpay_currency['code'] == currency)
-
-    return f"{count} BTC = {rate}{symbol}"
+    response = requests.get(f'https://bitpay.com/api/rates/{currency}').json()
+    rate = round(response['rate'] * int(count), 2)
+    symbol = next(bitpay_currency['symbol'] for bitpay_currency in url if bitpay_currency['code'] == currency)
+    return f'{count} BTC = {rate}{symbol} for the current date {datetime.now()}'
 
 
 if __name__ == '__main__':
