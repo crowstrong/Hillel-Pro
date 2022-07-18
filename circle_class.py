a838d13ebc1811dd60a5567df4d7bb051f6a0f99
Nikolay Tokariev <104066410+crowstrong@users.noreply.github.com>
2022-07-18 15:44:11 +0300

    Update circle_class.py

--- circle_class.py
+++ circle_class.py
@@ -14,8 +14,9 @@ def contains(self, point: Point) -> bool:
     return (point.x - self.center.x) ** 2 + (point.y - self.center.y) ** 2 <= self.radius ** 2
 
 
-circle = Circle(Point(0, 0), 2)
-print(contains(circle, Point(1, 1)))
-print(contains(circle, Point(-1, 3)))
-print(contains(circle, Point(-1, 3)))
-print(contains(circle, Point(1, 1)))
+if __name__ == '__main__':
+    circle = Circle(Point(0, 0), 2)
+    print(contains(circle, Point(1, 1)))
+    print(contains(circle, Point(-1, 3)))
+    print(contains(circle, Point(-1, 3)))
+    print(contains(circle, Point(1, 1)))
