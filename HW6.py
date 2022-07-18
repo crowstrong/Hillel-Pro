a234bb0cb27543e7a039812fca74fd1eacc36d5f
Nikolay Tokariev <104066410+crowstrong@users.noreply.github.com>
2022-07-18 15:14:32 +0300

    HW6 SQL + entering to OOP
    Продовження SQL. Старт з ООП.

---
+++ HW6.py
@@ -0,0 +1,43 @@
+from flask import Flask
+
+from webargs import fields, validate
+from webargs.flaskparser import use_kwargs
+
+from database_handler import execute_query, format_records
+
+app = Flask(__name__)
+
+
+@app.route('/stats_by_city')
+@use_kwargs(
+    {
+        "genre": fields.Str(
+            required=True,
+            validate=validate.Regexp("[a-zA-Z\s\W]")
+        )
+    },
+    location="query"
+)
+def stats_by_city(genre):
+    if genre:
+        query = f'''
+            SELECT BillingCountry, max(count)
+            FROM (
+            SELECT genres.Name, invoices.BillingCountry, COUNT(invoices.BillingCountry) as count
+            FROM genres
+            INNER JOIN tracks ON genres.GenreId = tracks.GenreId
+            INNER JOIN invoice_items ON tracks.TrackId = invoice_items.TrackId
+            INNER JOIN invoices ON invoice_items.InvoiceId = invoices.InvoiceId
+            GROUP BY genres.Name, invoices.BillingCountry
+            )
+            WHERE Name=\'{genre}\'
+            '''
+        genre_format = execute_query(query)
+        if genre_format[0][0] != None:
+            return format_records(genre_format)
+        else:
+            return f'Genre \'{genre}\' is not found'
+
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000, debug=True)
---
+++ circle_class.py
@@ -0,0 +1,21 @@
+class Point:
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+
+class Circle:
+    def __init__(self, center: Point, radius):
+        self.center = center
+        self.radius = radius
+
+
+def contains(self, point: Point) -> bool:
+    return (point.x - self.center.x) ** 2 + (point.y - self.center.y) ** 2 <= self.radius ** 2
+
+
+circle = Circle(Point(0, 0), 2)
+print(contains(circle, Point(1, 1)))
+print(contains(circle, Point(-1, 3)))
+print(contains(circle, Point(-1, 3)))
+print(contains(circle, Point(1, 1)))
